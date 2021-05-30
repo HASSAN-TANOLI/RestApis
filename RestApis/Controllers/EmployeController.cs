5e3284fa7f9015c9a4bffc3fdaf1a318d7e29dc5
Hassan-Tanoli22 <hassantanoli309@gmail.com>
2021-05-30 17:03:12 +0500

    second commit

---
+++ RestApis/Controllers/EmployeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestApis.Models;
+using RestApis.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeController : ControllerBase
+    {
+        private readonly IDataRepository<Employee> _dataRepository;
+        public EmployeController(IDataRepository<Employee> dataRepository)
+        {
+            _dataRepository = dataRepository;
+        }
+
+        // GET: api/Employee
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IEnumerable<Employee> employees = _dataRepository.GetAll();
+            return Ok(employees);
+        }
+
+        //GET: api/Employee/5
+        [HttpGet ("{id}", Name = "Get")]
+        public IActionResult Get(long id)
+        {
+            Employee employee = _dataRepository.Get(id);
+            if (employee == null)
+            {
+                return NotFound("Couldn't Found Employee Record");
+            }
+            return Ok(employee);
+        }
+
+        //Post: api/Employee
+        [HttpPost]
+        public IActionResult post([FromBody] Employee employee)
+        {
+            
+            if (employee == null)
+            {
+                return NotFound("Employee field is Null");
+            }
+            _dataRepository.Add(employee);
+            return CreatedAtRoute(
+                "Get",
+                new { Id = employee.EmployeeId },
+                employee);
+        }
+
+
+    }
+}
